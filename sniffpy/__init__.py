0e8b6bdfcdac06f5400b3967169225eef8589e61
Asif Mallik <asif.mallik1@gmail.com>
2020-07-18 14:49:53 -0400

    Fix resources typo

--- sniffpy/__init__.py
+++ sniffpy/__init__.py
@@ -51,15 +51,15 @@ def parse_mime_type(mime_type_string: str) -> MIMEType:
 def sniff(resource: bytes, mime_type_string: str = "unknown/unknown", no_sniff: bool = False, check_for_apache_bug: bool = False) -> str:
 	mime_type = parse_mime_type(mime_type_string)
 	if mime_type.is_unknown():
-		return sniff_unknown(resources, sniff_scriptable=not no_sniff)
+		return sniff_unknown(resource, sniff_scriptable=not no_sniff)
 	if no_sniff:
 		return mime_type
 	if check_for_apache_bug:
-		return sniff_mislabeled_binary(resources)
+		return sniff_mislabeled_binary(resource)
 	if mime_type.is_xml():
 		return mime_type
 	if mime_type.essence() == "text/html":
-		sniff_mislabeled_feed(resources)
+		sniff_mislabeled_feed(resource)
 	if mime_type.is_image(): #TODO: implement checking suppported image by user agent
 		match_type = match_image_type_pattern(resource)
 		if not match_type is None:
