ee57a1fef8a0b1cdc585d0fb431086f08d7890c7
Nicolas Beltran <nb2838@columbia.edu>
2020-07-29 16:10:30 -0500

    Added the match_pattern function

--- sniffpy/match.py
+++ sniffpy/match.py
@@ -1,7 +1,29 @@
-from .mimetype import MIMEType
+def match_pattern(resource: bytes, pattern: bytes, mask: bytes, ignored: bytes):
+    """
+    Implementation of algorithm in:
+    https://mimesniff.spec.whatwg.org/#matching-a-mime-type-pattern
+    True if pattern matches the resource. False otherwise.
+    """
+    resource = bytearray(resource)
+    pattern = bytearray(pattern)
+    mask = bytearray(mask)
 
-def match_image_type_pattern(resource: bytes) -> MIMEType:
-	raise NotImplementedError
+    if len(pattern) != len(mask):
+        return False
+    if len(resource) < len(pattern):
+        return False
 
-def match_video_audio_type_pattern(resource: bytes) -> MIMEType:
-	raise NotImplementedError
+    start = 0
+    for byte in resource:
+        if byte in ignored:
+            start += 1
+        else:
+            break
+
+    iteration_tuples = zip(resource[start:], pattern, mask)
+    for resource_byte, pattern_byte, mask_byte in iteration_tuples:
+        masked_byte = resource_byte & mask_byte
+        if masked_byte != pattern_byte:
+            return False
+
+    return True
