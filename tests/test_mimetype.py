c6679dd8c24e54385d7d035e40d95bd4946e0462
Asif Mallik <asif.mallik1@gmail.com>
2020-08-02 21:28:22 -0400

    Add test for parsing with unquoted single parameter

--- tests/test_mimetype.py
+++ tests/test_mimetype.py
@@ -10,3 +10,16 @@ def test_parse_mime_type_basic():
     
     assert mimetype_obj.type == "text"
     assert mimetype_obj.subtype == "html"
+
+def test_parse_mime_type_single_unquoted_parameter():
+    """Tests whether parsing works with a single unquoted parameter"""
+    
+    test_string = "text/html;charset=ISO-8859-1"
+    mimetype_obj = mimetype.parse_mime_type(test_string)
+
+    assert mimetype_obj.type == "text"
+    assert mimetype_obj.subtype == "html"
+    assert len(mimetype_obj.parameters) == 1 
+    assert "charset" in mimetype_obj.parameters
+    assert mimetype_obj.parameters['charset'] == "charset=ISO-8859-1"
+
