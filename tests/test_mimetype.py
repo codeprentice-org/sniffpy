a3fc3f245cd3644178e6fb48e23c9804ecdc0261
Asif Mallik <asif.mallik1@gmail.com>
2020-08-02 22:30:15 -0400

    Add quoted parameter test

--- tests/test_mimetype.py
+++ tests/test_mimetype.py
@@ -23,3 +23,15 @@ def test_parse_mime_type_single_unquoted_parameter():
     assert "charset" in mimetype_obj.parameters
     assert mimetype_obj.parameters['charset'] == "ISO-8859-1"
 
+def test_parse_mime_type_single_quoted_parameter():
+    """Tests whether parsing works with a single quoted parameter"""
+
+    test_string = 'text/html;charset="shift_jis"iso-2022-jpi'
+    mimetype_obj = mimetype.parse_mime_type(test_string)
+
+    assert mimetype_obj.type == "text"
+    assert mimetype_obj.subtype == "html"
+    assert len(mimetype_obj.parameters) == 1 
+    assert "charset" in mimetype_obj.parameters
+    assert mimetype_obj.parameters['charset'] == "shift_jis"
+
